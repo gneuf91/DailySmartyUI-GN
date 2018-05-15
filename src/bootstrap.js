2e39e154f30a2f24083c959351884c456226d67a
Gaven Neufeld <gneuf91@gmail.com>
2018-05-15 15:55:07 -0600

    added results.js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import reducers from './reducers';
 
@@ -11,15 +11,16 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 import 'bootstrap/dist/css/bootstrap.css';
 import './style/main.scss';
 
-import Home from './components/home'
+import Home from './components/home';
+import Results from './components/results';
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={Home}/>
-          <Route path="/results" component={Home}/>
+            <Route path='/' component={Home}/>
+            <Route path='/results' component={Results}/>
         </Switch>
       </BrowserRouter>
     </Provider>

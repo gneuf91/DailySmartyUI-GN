b3b9333c3bc0bd1cb0f2318821eb7c10ac54000e
Gaven Neufeld <gneuf91@gmail.com>
2018-05-16 15:06:51 -0600

    setup reducer for resultsPosts.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,7 @@
-import { SET_RECENT_POSTS } from './types';
+import { 
+    SET_RECENT_POSTS,
+    SET_RESULTS_POSTS
+} from './types';
 
 import axios from 'axios';
 
@@ -21,7 +24,7 @@ export function fetchPostsWithQuery(query) {
             .then(response => {
                 console.log(response.data.posts);
                 dispatch({
-                    type: SET_RECENT_POSTS,
+                    type: SET_RESULTS_POSTS,
                     payload: response.data.posts
                 })
             })

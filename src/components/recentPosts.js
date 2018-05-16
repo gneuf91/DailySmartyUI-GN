3c6097cc97360a6aeafc844e6671d70a7cb0811f
Gaven Neufeld <gneuf91@gmail.com>
2018-05-16 15:50:04 -0600

    added renderlink function to post.js

--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -17,7 +17,7 @@ class RecentPosts extends Component {
             
         if(index < 3) {
                 return (
-                    <Post {...post} key={index}/>
+                    <Post type="recent" {...post} key={index}/>
                 )
             }
         })                     
